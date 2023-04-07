7ab472b452ee421bd7e27b8e9fc9d869a1f1a370
cniscoding <asdfa.com>
2023-04-07 14:30:45 -0700

    add feature to admin dashboard to show count of product and categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: 'username', password: 'password'
 
   def show
+    @count_of_products = Product.count
+    @count_of_categories = Category.count
   end
 end
