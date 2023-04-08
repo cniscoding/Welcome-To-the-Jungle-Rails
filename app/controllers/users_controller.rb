5c229ec643fb03675dc4409142c8433d107415b4
cniscoding <asdfa.com>
2023-04-07 23:00:56 -0700

    added login logic and view pages and routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,8 +15,13 @@ class UsersController < ApplicationController
     private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(
+        :name,
+        :email, 
+        :password,
+        :password_confirmation
+      )
     end
-    
+
 end
 
