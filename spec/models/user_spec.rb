dfbdd7deb23edd08eb2c66bd794a1e499ee8caac
cniscoding <asdfa.com>
2023-04-11 16:44:10 -0700

    add testing spec for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'Validations' do
+    it 'user is valid if all field is input' do
+      @user = User.new(name: 'Jack', email: 'this@email.com', password: 'letmein', password_confirmation: 'letmein')
+      expect(@user).to be_valid
+    end
+    
+    it 'fails if password are not the same' do
+      @user = User.create(name: 'Jack', email: 'this@email.com', password: 'letmein', password_confirmation: 'dontletmein')
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+    it 'fails if either password is not input' do
+      @user = User.create(name: 'Jack', email: 'this@email.com', password: nil, password_confirmation: nil)
+      expect(@user.errors.full_messages).to include("Password can't be blank")
+    end
+
+    it 'the emails must be unique and not case sensitive or shows an error' do
+      @user = User.create(name: 'Jack', email: 'this@email.com', password: 'letmein', password_confirmation: 'letmein')
+      @user2 = User.create(name: 'Amble', email: 'this@email.com', password: 'letmein', password_confirmation: 'letmein')
+      expect(@user2.errors.full_messages).to include("Email has already been taken")
+    end
+
+    it 'shows an error when name is not input' do
+      @user = User.create(name: nil, email: 'this@email.com', password: 'letmein', password_confirmation: 'letmein')
+      expect(@user.errors.full_messages).to include("Name can't be blank")
+    end
+
+
+    it 'shows an error when email is not input' do
+      @user = User.create(name: 'Jack', email: nil,  password: 'letmein', password_confirmation: 'letmein')
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
+
+
+  end
 end
